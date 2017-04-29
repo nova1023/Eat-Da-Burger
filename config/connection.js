b807c45468a7307c69186d1ed4e89e53fc23fc76
nova1023 <thomas.yu1023@gmail.com>
2017-04-29 15:35:16 -0400

    corrected connections.js user, pw, host

--- config/connection.js
+++ config/connection.js
@@ -5,13 +5,12 @@ if (process.env.JAWSDB_URL) {
 	connection = mysql.createConnection(process.env.JAWSDB_URL);
 } else {
 	connection = ysql.createConnection({
-		host:"localhost",
-		user: "root",
-		password: "yourPassword",
-		database: "burgers_db"
+		host:"otmaa16c1i9nwrek.cbetxkdyhwsb.us-east-1.rds.amazonaws.com",
+		user: "ek0yrlt6z05xaqu3",
+		password: "lrb9xeivshww3wne",
+		database: "xmlvp05aks3y02tp"
 	});
 };
-
 
 connection.connect(function(err)
 {
